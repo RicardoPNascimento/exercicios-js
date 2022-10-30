0ebb8063aedcffefc18d857304c7ddf2928864ae
ricardo <ricardop794@gmail.com>
2022-10-30 14:31:42 -0300

    The exercise's answer

---
+++ Triangulo com Loop/zpractice.js
@@ -0,0 +1,51 @@
+// PRATICANDO FOR IN (object)
+
+// let dados = {
+//     nome: "ricardo",
+//     sobrenome: "nascimento",
+//     rua: "rua primeira",
+//     numero: 874
+// }
+// for (let n in dados){
+//     console.log(dados[n])
+// }
+
+// PRATICANDO FOR OF (arrays)
+
+// let data = [1, 'casa', 75, "ricardo"]
+//
+// for (let m of data) {
+//     console.log(m)
+// }
+
+//Exercício trianguloloop
+function loop() {
+    var str = "#";
+    let i = 0
+    while (i < 8) {
+        console.log(str.repeat(i));
+        i++;
+    }
+}
+
+loop()
+
+//PRATICANDO WHILE and DO-WHILE
+
+// let i = 0
+// while (i < 10){
+//     console.log(i)
+//     i++;
+// }
+
+// let i = 0
+// do {
+//     console.log(i)
+//     i++;
+// } while (i < 10)
+
+//PRATICANDO foreach
+// let animal = ['cat', 'dog', 'horse', 'bird']
+//
+// animal.forEach(animal => console.log(animal))
+
