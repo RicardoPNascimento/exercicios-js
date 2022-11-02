e5786e2e98f0b170d278166aa82ba949958175b7
ricardo <ricardop794@gmail.com>
2022-11-02 11:26:44 -0300

    The exercise's answer and deletede som pratice files

--- Minimo/code.js
+++ Minimo/code.js
@@ -1,5 +1,11 @@
-function minimo() {
-    // code
+function minimo(num1, num2) {
+    if (num1 < num2) {
+        return num1
+    } else if (num1 > num2) {
+        return num2
+    } else {
+        return 'numeros iguais'
+    }
 }
 
 module.exports = minimo
