d5047703c99058d77049d09746683d080da737dc
ricardo <ricardop794@gmail.com>
2022-11-07 13:30:34 -0300

    exercise's answer and pratice

--- Par ou Impar/code.js
+++ Par ou Impar/code.js
@@ -1,5 +1,15 @@
 function isEven(n) {
-    // code
+    // if (n === 0) {
+    //     return true
+    // } else if (n === 1) {
+    //     return false
+    // } else if (n < 0) {
+    //     n = n * -1
+    //     return isEven(n)
+    // } else {
+    //     n = n - 2
+    //     return isEven(n)
+    // }
 }
 
 module.exports = isEven
