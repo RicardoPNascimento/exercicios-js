d0cf4bf7c5ea5afaa0b8a1af54a765d1667fc839
ricardo <ricardop794@gmail.com>
2022-11-16 10:46:02 -0300

    Helper answer

--- Helper/code.js
+++ Helper/code.js
@@ -1,3 +1,17 @@
-// define your class here
+const isEven = require("../Par ou Impar/code");
+
+class NumberHelper {
+    static isEven(num) {
+        return isEven(num)
+    }
+
+    static isPositive(num) {
+        return num > 0
+    }
+
+    static isNegative(num) {
+        return num < 0
+    }
+}
 
 module.exports = NumberHelper
